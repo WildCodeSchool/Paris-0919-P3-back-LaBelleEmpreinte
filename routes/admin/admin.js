e29f3dda19d649a130494a104f3537bd46e5f613
Elgur <leroy.maxence@yahoo.com>
2019-12-16 11:55:45 +0100

    admin route02 post article created

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -3,7 +3,7 @@ const connection = require('../../conf')
 
 const router = express.Router()
 
-// route qui complète le dropdown menu des noms de table
+// route qui récupère les noms des tables et complète le dropdown menu avec
 router.get('/', (req, res) => {
     connection.query("SELECT table_name FROM information_schema.tables WHERE table_schema='la_belle_empreinte' AND table_name NOT LIKE '%has%';", (err, results) => {
         if (err) {
@@ -11,7 +11,19 @@ router.get('/', (req, res) => {
         } else {
             res.json(results)
         }
+    })
+    }
+)
 
+// post des données dans les input dans un tuple de la table article
+router.post('/articles', (req, res) => {
+    const postArticles = req.body
+    connection.query("INSERT INTO articles SET ? ", postArticles, (err, results) => {
+        if (err) {
+            res.status(500).send("l'aticle n'a pas pu être créé")
+        } else {
+            res.json(results)
+        }
     })
     }
 )

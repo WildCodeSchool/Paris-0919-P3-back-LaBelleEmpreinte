27e24feee37ff023dc99f15879f5f7cddad06a5b
Pauline Drouhin <pauline.drouhin@gmail.com>
2019-12-16 16:22:44 +0100

    request for multiple filters, working on filtering the result
    Co-authored-by: tracyZUMBU <zumbutracy@gmail.com>

--- routes/user/user.js
+++ routes/user/user.js
@@ -68,21 +68,35 @@ router.get('/objets', (req, res) => {
 
 })
 
-// code non terminé 
+// code non terminé //
 router.post('/filtre', (req, res) => {
     const objectId = req.body.objectId
     const besoinId = req.body.besoinId
     console.log(req.body)
     if(objectId && besoinId){
         connection.query(
-            ` SELECT besoins.besoins, articles.titre, articles.image, articles.id, categories_objets.categorie FROM articles_has_categories_objets INNER JOIN categories_objets ON articles_has_categories_objets.categories_objets_id = categories_objets.id INNER JOIN articles on articles.id = articles_has_categories_objets.articles_id INNER JOIN articles_has_besoins INNER JOIN besoins ON articles_has_besoins.besoins_id = besoins.id WHERE categories_objets.categorie = ? AND besoins.besoins = ? ` ,
-           [objectId, besoinId],
+            `SELECT besoins.besoins, articles.titre, articles.image, articles.id, categories_objets.categorie, types_activites.types_activites, objets.name, categories_intermediaires.name FROM articles_has_categories_objets INNER JOIN categories_objets ON articles_has_categories_objets.categories_objets_id = categories_objets.id INNER JOIN articles on articles.id = articles_has_categories_objets.articles_id INNER JOIN articles_has_besoins INNER JOIN besoins ON articles_has_besoins.besoins_id = besoins.id INNER JOIN articles_has_types_activites ON articles_has_types_activites.articles_id = articles.id INNER JOIN types_activites ON types_activites.id = articles_has_types_activites.types_activites_id INNER JOIN articles_has_categories_intermediaires ON articles.id = articles_has_categories_intermediaires.articles_id INNER JOIN categories_intermediaires ON categories_intermediaires.id = articles_has_categories_intermediaires.categories_intermediaires_id INNER JOIN articles_has_objets ON articles.id = articles_has_objets.articles_id INNER JOIN objets ON objets.id = articles_has_objets.objets_id WHERE categories_objets.categorie = ? OR categories_intermediaires.name = ? OR objets.name = ? AND besoins.besoins = ? OR types_activites.types_activites = ? `,
+            
+           [objectId,objectId, objectId, besoinId, besoinId] ,
            (err, results) => {
              if (err) {
+                 console.log(err)
                res
                  .status(500)
                  .send(`Erreur lors de la récupération de la liste des users !!`)
              } else {
+                const filteredArticles = results.map(article => {
+                    for (let i=0 ; i < filteredArticles.lenght ; i++) {
+                        if (article.id != filteredArticles.newArticle[i].id) {
+                            const newArticle = {
+                                titre : article.titre,
+                                image : article.image,
+                                id : article.id
+                            }
+                            filteredArticles.push(filteredArticles)
+                        }
+                    }
+                })
                res.status(200).json(results)
              }
            }

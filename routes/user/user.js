b6e5fa0b7b78b9a89e60a94b5d12159e7d61d211
Pauline Drouhin <pauline.drouhin@gmail.com>
2019-12-17 14:07:22 +0100

    reduced number or routes to get different filters of objects and needs

--- routes/user/user.js
+++ routes/user/user.js
@@ -3,69 +3,56 @@ const connection = require('../../conf')
 
 const router = express.Router()
 
-// Get besoins //
+// Get besoins et types d'activités //
 router.get('/besoins', (req, res) => {
 
     connection.query(`SELECT * FROM besoins`, (err, results) => {
         if (err) {
             res.status(500).send('Error retrieving besoins');
         } else {
-            res.json(results);
+            const besoins = {type: "besoins", results}
+            connection.query(`SELECT * FROM types_activites`, (err, results) => {
+                if (err) {
+                    res.status(500).send('Error retrieving types dactivites ');
+                } else {
+                   const typeActivites = {type: "types_activites", results}
+                   const filtreBesoin= [ besoins, typeActivites ]
+                    res.json(filtreBesoin);
+                }
+            });
         }
     });
 
 })
 
-// Get types d'activités //
-router.get('/activites', (req, res) => {
-
-    connection.query(`SELECT * FROM types_activites`, (err, results) => {
-        if (err) {
-            res.status(500).send('Error retrieving types dactivites ');
-        } else {
-            res.json(results);
-        }
-    });
-
-})
 
 // Get Catégories d'objets //
-router.get('/catobjets', (req, res) => {
+router.get('/objets', (req, res) => {
 
     connection.query(`SELECT * FROM categories_objets`, (err, results) => {
         if (err) {
             res.status(500).send('Error retrieving categories_objets');
         } else {
-            res.json(results);
-        }
-    });
+            const catObjets = {type: "Catégories d'Objets", results}
+            connection.query(`SELECT * FROM categories_intermediaires`, (err, results) => {
+                if (err) {
+                    res.status(500).send('Error retrieving categories_intermediaires ');
+                } else {
+                    const catInter = {type: "Catégories intermédiaires", results}
+                    connection.query(`SELECT * FROM objets`, (err, results) => {
+                        if (err) {
+                            res.status(500).send('Error retrieving objets');
+                        } else {
+                            const objets = {type: "Objets", results}
+                            const filtreObjet= [ catObjets, catInter, objets  ]
+                            res.json(filtreObjet);
+                        }
+                    }); 
+                }
+        })
+    };
 
 })
-
-// get Catégories intermédiaires //
-router.get('/catinter', (req, res) => {
-
-    connection.query(`SELECT * FROM categories_intermediaires`, (err, results) => {
-        if (err) {
-            res.status(500).send('Error retrieving categories_intermediaires ');
-        } else {
-            res.json(results);
-        }
-    });
-
-})
-
-// get objets //
-router.get('/objets', (req, res) => {
-
-    connection.query(`SELECT * FROM objets`, (err, results) => {
-        if (err) {
-            res.status(500).send('Error retrieving objets');
-        } else {
-            res.json(results);
-        }
-    });
-
 })
 
 // récupérer les articles selon les filtres sélectionnés //
@@ -77,7 +64,7 @@ router.post('/filtre', (req, res) => {
         connection.query(
             `SELECT besoins.besoins, articles.titre, articles.image, articles.id, categories_objets.categorie, types_activites.types_activites, objets.name, categories_intermediaires.name FROM articles_has_categories_objets INNER JOIN categories_objets ON articles_has_categories_objets.categories_objets_id = categories_objets.id INNER JOIN articles on articles.id = articles_has_categories_objets.articles_id INNER JOIN articles_has_besoins INNER JOIN besoins ON articles_has_besoins.besoins_id = besoins.id INNER JOIN articles_has_types_activites ON articles_has_types_activites.articles_id = articles.id INNER JOIN types_activites ON types_activites.id = articles_has_types_activites.types_activites_id INNER JOIN articles_has_categories_intermediaires ON articles.id = articles_has_categories_intermediaires.articles_id INNER JOIN categories_intermediaires ON categories_intermediaires.id = articles_has_categories_intermediaires.categories_intermediaires_id INNER JOIN articles_has_objets ON articles.id = articles_has_objets.articles_id INNER JOIN objets ON objets.id = articles_has_objets.objets_id WHERE categories_objets.categorie = ? OR categories_intermediaires.name = ? OR objets.name = ? AND besoins.besoins = ? OR types_activites.types_activites = ? `,
             
-           [objectId,objectId, objectId, besoinId, besoinId] ,
+           [objectId, objectId, objectId, besoinId, besoinId] ,
            (err, results) => {
              if (err) {
                  console.log(err)

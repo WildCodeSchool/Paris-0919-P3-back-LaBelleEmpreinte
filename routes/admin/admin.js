2739c5ac462dd1af1ee5b2ac70721a5a38bdf899
Pauline Drouhin <pauline.drouhin@gmail.com>
2020-01-07 18:19:39 +0100

    starting to work on route to get initiatives according to filters
    Co-authored-by: Elgurml <52967442+Elgurml@users.noreply.github.com>

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -2,6 +2,7 @@ const express = require('express')
 const connection = require('../../conf')
 const router = express.Router()
 
+//// PAGE ACCUEIL ADMIN ///
 // route qui récupère les noms des tables et complète le dropdown menu avec
 router.get('/', (req, res) => {
     connection.query("SELECT table_name FROM information_schema.tables WHERE table_schema='la_belle_empreinte' AND table_name NOT LIKE '%has%';", (err, results) => {
@@ -11,65 +12,25 @@ router.get('/', (req, res) => {
             res.json(results)
         }
     })
-    }
+}
 )
 
-/////////////// créer un article informatif /////////////////
-// post des données dans les input dans un tuple de la table article  ///  OK // bouton créer
+////// ARTICLE INFORMATIF ////////
 
-router.post('/articles', (req, res) => {
-    const postArticles = req.body.article
-    const initiatives = req.body.initiatives
-    const besoins = req.body.besoins
-    const types_activites = req.body.types_activites
-    const categories_objets = req.body.categories_objets
-    const categories_intermediaires = req.body.categories_intermediaires
-    const objets = req.body.objets
-    console.log(postArticles);
-
-    const tableBinding = ( tab1, tab2, tab2value, id ) => {
-        if ( tab2value.length != 0 ) {
-            tab2value.map( (elem, index) =>
-                connection.query(`INSERT INTO ${tab1}_has_${tab2} (${tab1}_id, ${tab2}_id) VALUES ( ${id}, ${elem})`, (err, results) => {
-                    if (err) { 
-                        res.status(500).send("l'article n'a pas pu être créé step 2")
-                    }
-                }))
-        } 
-    }
-    
-    connection.query("INSERT INTO articles SET ? ", postArticles, (err, results) => {
-        if (err) {
-            res.status(500).send("l'article n'a pas pu être créé")
-        } else {
-            const articleId = results.insertId
-            tableBinding('articles', 'initiatives', initiatives, articleId)
-            tableBinding('articles', 'besoins', besoins, articleId)
-            tableBinding('articles', 'types_activites', types_activites, articleId)
-            tableBinding('articles', 'categories_objets', categories_objets, articleId)
-            tableBinding('articles', 'categories_intermediaires', categories_intermediaires, articleId)
-            tableBinding('articles', 'objets', objets, articleId)
-            res.status(200).send('article créé')  /// faut qu'on trouve un moyen de faire le res.status(200) après que les fonctions soient jouées
-        }
-        
-    })
-    }
-)
-
-//// afficher les objets et besoins dans les dropdown menu correspondants
+//// afficher les objets et besoins dans les dropdown menu correspondants ///////////
 // Get besoins et types d'activités //
 router.get('/besoins', (req, res) => {
     connection.query(`SELECT * FROM besoins`, (err, results) => {
         if (err) {
             res.status(500).send('Error retrieving besoins');
         } else {
-            const besoins = {type: "besoins", results}
+            const besoins = { type: "besoins", results }
             connection.query(`SELECT * FROM types_activites`, (err, results) => {
                 if (err) {
                     res.status(500).send('Error retrieving types dactivites ');
                 } else {
-                    const typeActivites = {type: "types_activites", results}
-                    const filtreBesoin= [ besoins, typeActivites ]
+                    const typeActivites = { type: "types_activites", results }
+                    const filtreBesoin = [besoins, typeActivites]
                     res.json(filtreBesoin);
                 }
             });
@@ -85,26 +46,103 @@ router.get('/objets', (req, res) => {
         if (err) {
             res.status(500).send('Error retrieving categories_objets');
         } else {
-            const catObjets = {type: "Catégories d'Objets", results}
+            const catObjets = { type: "Catégories d'Objets", results }
             connection.query(`SELECT * FROM categories_intermediaires`, (err, results) => {
                 if (err) {
                     res.status(500).send('Error retrieving categories_intermediaires ');
                 } else {
-                    const catInter = {type: "Catégories intermédiaires", results}
+                    const catInter = { type: "Catégories intermédiaires", results }
                     connection.query(`SELECT * FROM objets`, (err, results) => {
                         if (err) {
                             res.status(500).send('Error retrieving objets');
                         } else {
-                            const objets = {type: "Objets", results}
-                            const filtreObjet= [ catObjets, catInter, objets  ]
+                            const objets = { type: "Objets", results }
+                            const filtreObjet = [catObjets, catInter, objets]
                             res.json(filtreObjet);
                         }
-                    }); 
+                    });
                 }
-        })
-    };
+            })
+        };
+    })
 })
+
+/// Récupérer la liste des initiatives //// 
+
+router.post('/filtre', (req, res) => {
+    const besoins = req.body.besoins
+    const types_activites = req.body.types_activites
+    const categories_objets = req.body.categories_objets
+    const categories_intermediaires = req.body.categories_intermediaires
+    const objets = req.body.objets
+    const filteredInitiatives = []
+    const getFilters = (tab, tabValue) => {
+        console.log(filteredInitiatives)
+        if (tabValue.length != 0) {
+            tabValue.map((elem, index) =>
+                connection.query(`SELECT initiatives.name, initiatives.id FROM initiatives INNER JOIN initiatives_has_${tab} ON initiatives_has_${tab}.initiatives_id = initiatives.id WHERE initiatives_has_${tab}.${tab}_id = ${tabValue} GROUP BY initiatives.id`, (err, results) => {
+                    if (err) {
+                        res.status(500).send(`les initiatives associées à ${tab} n'ont pas été récupérées`)
+                    }
+                    else {
+                        const uniqueFilteredInitiatives = [...new Set(results.map(elem => filteredInitiatives.elem))]
+                        console.log('unique', uniqueFilteredInitiatives)
+                    }
+                    // const uniqueFilteredInitiatives = [...new Set(results.map(elem => filteredInitiatives.elem))]
+                }))
+        }
+    }
+
+    getFilters('besoins', besoins)
+    getFilters('objets', objets)
+    res.status(200).send(uniqueFilteredInitiatives)
+
+
+   // SELECT initiatives.name, initiatives.id FROM initiatives INNER JOIN initiatives_has_besoins ON initiatives_has_besoins.initiatives_id = initiatives.id INNER JOIN besoins ON initiatives_has_besoins.initiatives_id = initiatives.id WHERE besoins.id = 4 GROUP BY initiatives.id // 
 })
+
+/////////////// créer un article informatif /////////////////
+// post des données dans les input dans un tuple de la table article  ///  OK // bouton créer
+router.post('/articles/create', (req, res) => {
+    const postArticles = req.body.article
+    const initiatives = req.body.initiatives
+    const besoins = req.body.besoins
+    const types_activites = req.body.types_activites
+    const categories_objets = req.body.categories_objets
+    const categories_intermediaires = req.body.categories_intermediaires
+    const objets = req.body.objets
+    console.log(postArticles);
+
+    const tableBinding = (tab1, tab2, tab2value, id) => {
+        if (tab2value.length != 0) {
+            tab2value.map((elem, index) =>
+                connection.query(`INSERT INTO ${tab1}_has_${tab2} (${tab1}_id, ${tab2}_id) VALUES ( ${id}, ${elem})`, (err, results) => {
+                    if (err) {
+                        res.status(500).send("l'article n'a pas pu être créé step 2")
+                    }
+                }))
+        }
+    }
+
+    connection.query("INSERT INTO articles SET ? ", postArticles, (err, results) => {
+        if (err) {
+            res.status(500).send("l'article n'a pas pu être créé")
+        } else {
+            const articleId = results.insertId
+            tableBinding('articles', 'initiatives', initiatives, articleId)
+            tableBinding('articles', 'besoins', besoins, articleId)
+            tableBinding('articles', 'types_activites', types_activites, articleId)
+            tableBinding('articles', 'categories_objets', categories_objets, articleId)
+            tableBinding('articles', 'categories_intermediaires', categories_intermediaires, articleId)
+            tableBinding('articles', 'objets', objets, articleId)
+            res.status(200).send('article créé')  /// faut qu'on trouve un moyen de faire le res.status(200) après que les fonctions soient jouées
+        }
+
+    })
+}
+)
+
+
 
 
 //// selectioner les critères à asssocier
@@ -135,10 +173,10 @@ router.post('/filtre', (req, res) => {
         }
     })
 })    // à supprimer quand on va uncomment
-    
-    
+
+
 //     objectsList.map(object => 
-        
+
 //         )
 //     if(objectsList && besoinsList){
 //         connection.query(
@@ -154,7 +192,7 @@ router.post('/filtre', (req, res) => {
 //                 const filteredArticles = []
 //                 results.map(article => {
 //                     if (filteredArticles.length == 0) {
-                        
+
 //                         const newArticle = {
 //                             titre : article.titre,
 //                             image : article.image,
@@ -192,7 +230,7 @@ router.post('/filtre', (req, res) => {
 //                 const filteredArticles = []
 //                 results.map(article => {
 //                     if (filteredArticles.length == 0) {
-                        
+
 //                         const newArticle = {
 //                             titre : article.titre,
 //                             image : article.image,
@@ -230,7 +268,7 @@ router.post('/filtre', (req, res) => {
 //                 const filteredArticles = []
 //                 results.map(article => {
 //                     if (filteredArticles.length == 0) {
-                        
+
 //                         const newArticle = {
 //                             titre : article.titre,
 //                             image : article.image,
@@ -257,7 +295,7 @@ router.post('/filtre', (req, res) => {
 //     }
 //   })
 
- ////////////// afficher les articles informatifs ///////////////           à voir avec Pauline et Tracy
+////////////// afficher les articles informatifs ///////////////           à voir avec Pauline et Tracy
 // router.get('/articles_liste', (req, res) => {
 //     connection.query('SELECT id, titres, publication FROM articles WHERE ',, (err, results) => {
 //         if (err) {
@@ -281,7 +319,7 @@ router.get('/articles_maj/:id', (req, res) => {
             res.status(200).json(results)
         }
     })
-    }
+}
 )
 
 // modifier un article informatif               OK
@@ -295,7 +333,7 @@ router.put('/articles_maj/:id', (req, res) => {
             res.status(200).send('article modifié')
         }
     })
-    }
+}
 )
 
 // supprimer un article informatif          OK
@@ -308,10 +346,10 @@ router.delete('/articles_maj/:id', (req, res) => {
             res.status(200).send('article supprimé')
         }
     })
-    }
+}
 )
 
- ////////////// afficher les initiatives responsables ///////////////            à voir avec Pauline et Tracy
+////////////// afficher les initiatives responsables ///////////////            à voir avec Pauline et Tracy
 //  router.get('/initiatives_liste', (req, res) => {
 //     connection.query('SELECT id, titres, publication FROM initiatives WHERE ',, (err, results) => {
 //         if (err) {
@@ -334,7 +372,7 @@ router.get('/initiatives_maj/:id', (req, res) => {
             res.status(200).json(results)
         }
     })
-    }
+}
 )
 
 // modifier une initiative responsable               OK
@@ -348,7 +386,7 @@ router.put('/initiatives_maj/:id', (req, res) => {
             res.status(200).send('initiative modifiée')
         }
     })
-    }
+}
 )
 
 // supprimer une initiative responsable          OK
@@ -361,7 +399,7 @@ router.delete('/initiatives_maj/:id', (req, res) => {
             res.status(200).send('initiative supprimée')
         }
     })
-    }
+}
 )
 
 
